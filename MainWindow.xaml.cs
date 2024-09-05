eba0606882761f20916127515c6d3bc89e216075
samet <sametcn99@gmail.com>
2024-09-05 05:41:56 +0300

    Update UI, threading, and file-saving logic in MainWindow
    - Changed `Background` of `StackPanel` in `MainWindow.xaml` to `#202020`
    - Added `CacheMode="BitmapCache"` to `ColorPicker` control
    - Added `using` directives for `Microsoft.UI.Windowing` and `System.Threading`
    - Introduced `SemaphoreSlim` for thread safety in `MainWindow` class
    - Adjusted window size in `MainWindow` constructor to `400x600`
    - Removed comments and constants related to desktop wallpaper
    - Updated `ColorPicker_ColorChanged` to use `semaphoreSlim`
    - Changed bitmap saving to save directly to `PicturesLibrary`
    - Updated `SystemParametersInfo` to use new file path
    - Improved debugging and error handling for new file-saving process
    - Ensured semaphore release in `finally` block of `ColorPicker_ColorChanged`

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
@@ -7,20 +8,17 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Windows.Storage;
 using Windows.UI.Popups;
 using WinRT.Interop;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace SolidColorBackground
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
         public MainWindow()
         {
             this.InitializeComponent();
@@ -28,80 +26,53 @@ namespace SolidColorBackground
             // Set window size and position
             var hwnd = WindowNative.GetWindowHandle(this);
             var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
-            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-            appWindow.Resize(new Windows.Graphics.SizeInt32(370, 600));
-
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32(400, 600));
         }
 
-        /// <summary>
-        /// Sets the desktop wallpaper using the specified parameters.
-        /// </summary>
-        /// <param name="uAction">The action to perform.</param>
-        /// <param name="uParam">Additional parameters for the action.</param>
-        /// <param name="lpvParam">The parameter value.</param>
-        /// <param name="fuWinIni">The flags that specify how the change should be applied.</param>
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
-        /// <summary>
-        /// The constant value for setting the desktop wallpaper.
-        /// </summary>
         const int SPI_SETDESKWALLPAPER = 20;
-        /// <summary>
-        /// The constant value for updating the INI file.
-        /// </summary>
         const int SPIF_UPDATEINIFILE = 0x01;
-        /// <summary>
-        /// The constant value for sending a change notification.
-        /// </summary>
         const int SPIF_SENDCHANGE = 0x02;
 
         private readonly Bitmap bitmap = new Bitmap(1, 1);
 
-        /// <summary>
-        /// Event handler for the ColorChanged event of the ColorPicker control.
-        /// </summary>
-        /// <param name="sender">The ColorPicker control that raised the event.</param>
-        /// <param name="args">The event arguments.</param>
         private async void ColorPicker_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
         {
+            await semaphoreSlim.WaitAsync();
             try
             {
-                // get the color from the color picker
                 var color = colorPicker.Color;
                 var solidColor = Color.FromArgb(color.A, color.R, color.G, color.B);
 
-                // set the pixel color of the bitmap
                 bitmap.SetPixel(0, 0, solidColor);
 
-                // save the bitmap to a memory stream
                 using (var memoryStream = new MemoryStream())
                 {
                     bitmap.Save(memoryStream, ImageFormat.Png);
                     memoryStream.Seek(0, SeekOrigin.Begin);
 
-                    // save the memory stream to a temporary file
-                    var tempFile = Path.GetTempFileName();
-                    using (var fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write))
+                    var picturesFolder = await KnownFolders.PicturesLibrary.CreateFileAsync("solid_color_background.png", CreationCollisionOption.ReplaceExisting);
+                    using (var fileStream = await picturesFolder.OpenStreamForWriteAsync())
                     {
                         await memoryStream.CopyToAsync(fileStream);
                     }
 
-                    // set the bitmap as the windows background
-                    var file = await StorageFile.GetFileFromPathAsync(tempFile);
-                    Debug.WriteLine(file.Path);
+                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, picturesFolder.Path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
 
-                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file.Path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
-
-                    // cleanup the temporary file
-                    File.Delete(tempFile);
+                    Debug.WriteLine($"File saved to: {picturesFolder.Path}");
                 }
             }
             catch (Exception ex)
             {
-                // log the exception details
                 var messageDialog = new MessageDialog($"An error occurred while setting the background color: {ex.Message}");
                 await messageDialog.ShowAsync();
+            }
+            finally
+            {
+                semaphoreSlim.Release();
             }
         }
     }
